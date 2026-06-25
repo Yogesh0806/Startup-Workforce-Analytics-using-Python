76ff48c02948a8efc62ec98a54718b82f5e4c08b
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-06-25 23:51:37 +0530

    Started data understanding and inspection

---
+++ workforce_analytics.py
@@ -0,0 +1,11 @@
+# Import Libraries
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the dataset
+
+df = pd.read_csv(r"C:\Users\HP\OneDrive\Documents\Desktop\Startup Workforce Analytics using Python\employee_data.csv")
+
+print(df.head())
