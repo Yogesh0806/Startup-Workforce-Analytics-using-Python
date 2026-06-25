f79305eddd25eb70c2c33d1c291aaed60b3628b4
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-06-26 00:00:00 +0530

    day 1 complete

--- workforce_analytics.py
+++ workforce_analytics.py
@@ -8,4 +8,24 @@ import seaborn as sns
 
 df = pd.read_csv(r"C:\Users\HP\OneDrive\Documents\Desktop\Startup Workforce Analytics using Python\employee_data.csv")
 
-print(df.head())
+# print(df.head())
+# ID Gender  Experience (Years)               Position  Salary
+# 0   1      F                   4        DevOps Engineer  109976
+# 1   2      M                   6        DevOps Engineer  120088
+# 2   3      M                  17          Web Developer  181301
+# 3   4      M                   7  Systems Administrator   77530
+# 4   5      F                  13  Systems Administrator  152397
+
+# print(df.info())
+
+# print(df.shape)
+    # (400, 5)
+    
+# chack for null values 
+# print(df.isnull().sum())
+
+# duplicate values 
+# print(df.duplicated().sum())
+
+# chack data types of the column
+print(df.dtypes())
