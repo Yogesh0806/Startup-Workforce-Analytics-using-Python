c3b914a73a9b5ac1248e8fa1cb0918ea29705ab6
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-06-27 23:18:06 +0530

    Insight 2 of this project

--- workforce_insights.py
+++ workforce_insights.py
@@ -34,3 +34,28 @@ lowest_salary= salary_position.min()
 # print(f"Insight 1: {highest_position} has the highest average salary (₹{highest_salary:,.2f}).")
 # print(f"Insight 2: {lowest_position} has the lowest average salary (₹{lowest_salary:,.2f}), showing a clear salary difference across positions.")
 
+'''Insight 2: Average Experience by Position'''
+
+experience_position = df.groupby('Position')['Experience (Years)'].mean().sort_values(ascending=False)
+# print(experience_position)
+
+# Visualization
+
+# plt.figure(figsize=(10,6))
+# experience_position.plot(kind='barh')
+# plt.title('Average Experience by Position')
+# plt.xlabel('Experience of years')
+# plt.yticks(rotation=45)
+# plt.show()
+
+# Top 2 Insights
+
+highest_exp_position = experience_position.idxmax()
+highest_exp = experience_position.max()
+
+lowest_exp_position = experience_position.idxmin()
+lowest_exp = experience_position.min()
+
+# print(f"Insight 1: {highest_exp_position} has the highest average experience ({highest_exp:.1f} years).")
+
+# print(f"Insight 2: {lowest_exp_position} has the lowest average experience ({lowest_exp:.1f} years), indicating that this role is generally filled by less experienced employees.")
