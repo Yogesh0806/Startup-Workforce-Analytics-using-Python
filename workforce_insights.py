065bc4efb3040024eed140e0759449cc91f1ae2a
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-06-27 23:34:07 +0530

    Insight 3 of this project

--- workforce_insights.py
+++ workforce_insights.py
@@ -59,3 +59,38 @@ lowest_exp = experience_position.min()
 # print(f"Insight 1: {highest_exp_position} has the highest average experience ({highest_exp:.1f} years).")
 
 # print(f"Insight 2: {lowest_exp_position} has the lowest average experience ({lowest_exp:.1f} years), indicating that this role is generally filled by less experienced employees.")
+
+
+'''Insight 3: Gender Representation Across Positions'''
+
+gender_position = pd.crosstab(df['Position'], df['Gender'])
+
+# print(gender_position)
+
+# Visualization
+
+# gender_position.plot(kind='bar',figsize=(12,6))
+# plt.title('Gender Distribution Across Positions')
+# plt.ylabel('Employee Count')
+# plt.xticks(rotation=30)
+# plt.show()
+
+
+# Top 2 Insights
+
+gender_count = df['Gender'].value_counts()
+
+majority_gender = gender_count.idxmax()
+majority_count = gender_count.max()
+
+if majority_gender == 'M':
+    majority_gender = 'Male'
+elif majority_gender == 'F':
+    majority_gender = 'Female'
+    
+minority_gender = gender_count.idxmin()
+minority_count = gender_count.min()
+
+print(f"Insight 1: {majority_gender} employees form the largest workforce group ({majority_count} employees).")
+
+print(f"Insight 2: Both genders are represented across multiple positions, indicating workforce diversity.")
